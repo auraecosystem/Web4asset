4dfcc2716f0782bee95ca0d1b0e12fa5a1911172
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-16 08:38:56 +0100

    Add files via upload

--- Web4asset.sol
+++ Web4asset.sol
@@ -2,15 +2,37 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Web4Asset is ERC721 {
+contract Web4Asset is ERC721, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
     uint256 private _tokenIds;
 
-    constructor() ERC721("Web4Asset", "W4A") {}
+    constructor(address admin) ERC721("Web4Asset", "W4A") {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(MINTER_ROLE, admin);
+        _grantRole(BURNER_ROLE, admin);
+    }
 
-    function mint(address to) public returns (uint256) {
+    function mint(address to) public onlyRole(MINTER_ROLE) returns (uint256) {
         _tokenIds++;
         _mint(to, _tokenIds);
         return _tokenIds;
+    }
+
+    function burn(uint256 tokenId) public onlyRole(BURNER_ROLE) {
+        _burn(tokenId);
+    }
+
+    // Required override since both ERC721 and AccessControl implement supportsInterface
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
