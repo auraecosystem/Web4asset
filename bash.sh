d2fec0ff55cb587ec37b47aeeb6ee2830a879df1
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-16 08:24:31 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,2 +1,11 @@
+git clone https://github.com/auraecosystem/Web4asset.git
+cd Web4asset
+# paste the updated contract into Web4asset.sol
+git checkout -b feat/rbac-access-control
+git add Web4asset.sol
+git commit -m "Add RBAC (MINTER_ROLE, BURNER_ROLE) via AccessControl"
+git push origin feat/rbac-access-control
+# then open a PR on GitHub
+
 https://github.com/auraecosystem/Web4asset.git
 cd web4-asset-1747416169968
