d7c6e66f6efd40aa81197371cd274f66d26460d3
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-17 12:03:04 +0100

    Update Web4asset.sol

--- contracts/Web4asset.sol
+++ contracts/Web4asset.sol
@@ -1,38 +1,9 @@
+// contracts/MyNFT.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Web4Asset is ERC721, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
-
-    uint256 private _tokenIds;
-
-    constructor(address admin) ERC721("Web4Asset", "W4A") {
-        _grantRole(DEFAULT_ADMIN_ROLE, admin);
-        _grantRole(MINTER_ROLE, admin);
-        _grantRole(BURNER_ROLE, admin);
-    }
-
-    function mint(address to) public onlyRole(MINTER_ROLE) returns (uint256) {
-        _tokenIds++;
-        _mint(to, _tokenIds);
-        return _tokenIds;
-    }
-
-    function burn(uint256 tokenId) public onlyRole(BURNER_ROLE) {
-        _burn(tokenId);
-    }
-
-    // Required override since both ERC721 and AccessControl implement supportsInterface
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, AccessControl)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+contract MyNFT is ERC721 {
+    constructor() ERC721("MyNFT", "MNFT") {}
 }
